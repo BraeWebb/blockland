f30f2df150615e68862c3b31e4c74aa70d65e508
BraeWebb <email@braewebb.com>
2018-08-29 20:53:55 +1000

    Finished Tile Tests

--- test/TileTest.java
+++ test/TileTest.java
@@ -143,4 +143,174 @@ public class TileTest {
             Assert.fail();
         } catch (TooLowException e) {}
     }
+
+    @Test
+    public void testRemoveExit() {
+        Tile leftTile = new Tile();
+        try {
+            tile.addExit("left", leftTile);
+        } catch (NoExitException e) {
+            Assert.fail();
+        }
+
+        Assert.assertEquals(1, tile.getExists().size());
+        Assert.assertEquals(leftTile, tile.getExists().get("left"));
+
+        try {
+            tile.removeExit("right");
+            Assert.fail();
+        } catch (NoExitException e) {}
+
+        try {
+            tile.removeExit(null);
+            Assert.fail();
+        } catch (NoExitException e) {}
+
+        try {
+            tile.removeExit("left");
+        } catch (NoExitException e) {
+            Assert.fail();
+        }
+
+        Assert.assertEquals(0, tile.getExists().size());
+    }
+
+    @Test
+    public void testDig() {
+        try {
+            emptyTile.dig();
+            Assert.fail();
+        } catch (TooLowException e) {
+
+        } catch (InvalidBlockException e) {
+            Assert.fail();
+        }
+
+        try {
+            Block topBlock = tile.dig();
+            Block expectedBlock = startingBlocks.get(startingBlocks.size() - 1);
+
+            Assert.assertEquals(expectedBlock, topBlock);
+        } catch (TooLowException|InvalidBlockException e) {
+            Assert.fail();
+        }
+
+        startingBlocks = new ArrayList<>();
+        startingBlocks.add(new SoilBlock());
+        startingBlocks.add(new SoilBlock());
+        startingBlocks.add(new StoneBlock());
+        Tile noDigTile;
+        try {
+            noDigTile = new Tile(startingBlocks);
+        } catch (TooHighException e) {
+            Assert.fail();
+            return;
+        }
+
+        try {
+            noDigTile.dig();
+            Assert.fail();
+        } catch (TooLowException e) {
+            Assert.fail();
+        } catch (InvalidBlockException e) {
+
+        }
+    }
+
+    @Test
+    public void testMoveBlock() {
+        // create exits for testing
+        try {
+            tile.addExit("tall", tallTile);
+            tile.addExit("empty", emptyTile);
+            tallTile.addExit("tall", tile);
+        } catch (NoExitException e) {
+            Assert.fail();
+        }
+
+        // exit name that doesn't exist causes
+        try {
+            tile.moveBlock("right");
+            Assert.fail();
+        } catch (TooHighException|InvalidBlockException e) {
+            Assert.fail();
+        } catch (NoExitException e) {}
+
+        // null exist name causes NoExitException
+        try {
+            tile.moveBlock(null);
+            Assert.fail();
+        } catch (TooHighException|InvalidBlockException e) {
+            Assert.fail();
+        } catch (NoExitException e) {}
+
+        // moving to a taller block causes TooHighException
+        try {
+            tile.moveBlock("tall");
+            Assert.fail();
+        } catch (NoExitException|InvalidBlockException e) {
+            Assert.fail();
+        } catch (TooHighException e) {}
+
+        // trying to move a grass block causes an InvalidBlockException
+        try {
+            tile.moveBlock("empty");
+            Assert.fail();
+        } catch (NoExitException|TooHighException e) {
+            Assert.fail();
+        } catch (InvalidBlockException e) {}
+
+
+        // successfully move a tile from one block to another
+        try {
+            Assert.assertEquals(7, tallTile.getBlocks().size());
+            Assert.assertEquals(3, tile.getBlocks().size());
+            tallTile.moveBlock("tall");
+            Assert.assertEquals(6, tallTile.getBlocks().size());
+            Assert.assertEquals(4, tile.getBlocks().size());
+        } catch (NoExitException|TooHighException|InvalidBlockException e) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testPlaceBlock() {
+        Block newBlock = new WoodBlock();
+        Block topBlock = startingBlocks.get(startingBlocks.size() - 1);
+
+        try {
+            Assert.assertEquals(7, tallTile.getBlocks().size());
+            Assert.assertEquals(topBlock, tallTile.getTopBlock());
+
+            tallTile.placeBlock(newBlock);
+
+            Assert.assertEquals(8, tallTile.getBlocks().size());
+            Assert.assertEquals(newBlock, tallTile.getTopBlock());
+        } catch (TooHighException|TooLowException|InvalidBlockException e) {
+            Assert.fail();
+        }
+
+        try {
+            tile.placeBlock(null);
+            Assert.fail();
+        } catch (TooHighException e) {
+            Assert.fail();
+        } catch (InvalidBlockException e) {}
+
+        try {
+            tallTile.placeBlock(newBlock);
+            Assert.fail();
+        } catch (TooHighException e) {
+        } catch (InvalidBlockException e) {
+            Assert.fail();
+        }
+
+        try {
+            tile.placeBlock(new GrassBlock());
+            Assert.fail();
+        } catch (TooHighException e) {
+        } catch (InvalidBlockException e) {
+            Assert.fail();
+        }
+    }
 }
